682707083c022c84f06b1ea08b81d5ddd1f268d0
Aidar <ar.ibraev98@gmail.com>
2024-01-17 20:00:13 +0600

    Update styling

--- 0_select_basics.sql
+++ 0_select_basics.sql
@@ -10,15 +10,18 @@ Angola	Africa	1246700	20609294	100990000000
 
 -- 1. Modify table to show the population of Germany 
 
-SELECT population FROM world
+SELECT population 
+  FROM world
   WHERE name = 'Germany';
 
 -- 2. Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
 
-SELECT name, population FROM world
+SELECT name, population 
+  FROM world
   WHERE name in ('Sweden', 'Norway', 'Denmark');
 
 -- 3. Modify table to show the country and the area for countries with an area between 200,000 and 250,000.
 
-SELECT name, area FROM world
+SELECT name, area 
+  FROM world
   WHERE area BETWEEN 200000 AND 250000;
--- 1_select_name.sql
+++ 1_select_name.sql
@@ -39,45 +39,54 @@ SELECT name FROM world
 
 -- 7. Find the countries that have three or more a in the name
 
-SELECT name FROM world
+SELECT name 
+  FROM world
   WHERE name LIKE '%a%a%a%';
 
 -- 8. Find the countries that have "t" as the second character.
 
-SELECT name FROM world
+SELECT name 
+  FROM world
   WHERE name LIKE '_t%';
 
 -- 9. Find the countries that have two "o" characters separated by two others.
 
-SELECT name FROM world
+SELECT name 
+  FROM world
   WHERE name LIKE '%o__o%';
 
 -- 10. Find the countries that have exactly four characters.
 
-SELECT name FROM world
+SELECT name 
+  FROM world
   WHERE name LIKE '____';
 
 -- 11. Find the country where the name is the capital city.
 
-SELECT name FROM world
+SELECT name 
+  FROM world
   WHERE name = capital;
 
 -- 12. The capital of Mexico is Mexico City. Show all the countries where the capital has the country together with the word "City". Find the country where the capital is the country plus "City".
 
-SELECT name FROM world
+SELECT name 
+  FROM world
   WHERE capital = CONCAT(name, ' City');
 
 -- 13. Find the capital and the name where the capital includes the name of the country.
 
-SELECT capital, name FROM world
+SELECT capital, name 
+  FROM world
   WHERE capital LIKE CONCAT('%', name, '%');
 
 -- 14. Find the capital and the name where the capital is an extension of name of the country.
 
-SELECT capital, name FROM world
+SELECT capital, name 
+  FROM world
   WHERE capital LIKE CONCAT('%', name, '%') AND LENGTH(capital) > LENGTH(name);
 
 -- 15. Show the name and the extension where the capital is a proper (non-empty) extension of name of the country.  
 
-SELECT name, REPLACE(capital, name, '') AS extension FROM world
+SELECT name, REPLACE(capital, name, '') AS extension 
+  FROM world
   WHERE LENGTH(capital) > LENGTH(name) AND capital LIKE CONCAT('%', name, '%');
