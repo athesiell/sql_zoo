9003b4264a1d26c885c45a7338d7463201cd9c41
Aidar <ar.ibraev98@gmail.com>
2024-01-17 20:21:33 +0600

    Fix styling

--- 0_select_basics.sql
+++ 0_select_basics.sql
@@ -12,16 +12,16 @@ Angola	Africa	1246700	20609294	100990000000
 
 SELECT population 
   FROM world
-  WHERE name = 'Germany';
+ WHERE name = 'Germany';
 
 -- 2. Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
 
 SELECT name, population 
   FROM world
-  WHERE name in ('Sweden', 'Norway', 'Denmark');
+ WHERE name in ('Sweden', 'Norway', 'Denmark');
 
 -- 3. Modify table to show the country and the area for countries with an area between 200,000 and 250,000.
 
 SELECT name, area 
   FROM world
-  WHERE area BETWEEN 200000 AND 250000;
+ WHERE area BETWEEN 200000 AND 250000;
